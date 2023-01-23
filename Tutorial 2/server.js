398b45e09d3ec9a4b2479b9c7f6a48394d7b4403
Tony Bonki <114664225+Tonjaro@users.noreply.github.com>
2023-01-23 15:19:36 +0000

    edited server.js and index.html for server prac

--- Tutorial 2/server.js
+++ Tutorial 2/server.js
@@ -1,9 +1,22 @@
 //Require a HTTP Module
 const http = require('http');
-
+const fs = require('fs')
 // Create a server
 const server = http.createServer((request, response)=>{
-    console.log('request made')
+    console.log(request.url, request.method)
+
+    // set header content type
+    response.setHeader('Content-Type', 'text/html')
+
+   // send a html file
+   fs.readFile('./views/index.html', (err, data)=>{
+    if (err){
+        console.log(err)
+        response.end()
+    }else{
+        response.end(data)
+    }
+   })
 });
 
 server.listen(3000, 'localhost', ()=>{
@@ -15,3 +28,4 @@ Local host is like domain names but takes us to a loop back ip adress 127.0.0.1 
 Port Numbers are like doors into a computer where different communications can be made to different programs.
 Port 3000 is a common port for local web development servers.
 */
+
