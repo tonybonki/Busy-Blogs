be84511a130526dd9fc2595985d10aa833eae12f
Tony Bonki <114664225+Tonjaro@users.noreply.github.com>
2023-01-24 14:07:09 +0000

    installed ejs package

---
+++ Tutorial 2/app.js
@@ -0,0 +1,23 @@
+// Using Express
+const express =  require('express') 
+
+// Express app
+
+const app = express()
+
+// listen for requests 
+app.listen(3000)
+
+app.get('/', (request, response)=>{
+    response.sendFile('./views/index.html', {root: __dirname})
+})
+
+app.get('/about', (request, response)=>{
+    response.sendFile('./views/about.html', {root: __dirname})
+})
+
+//404 page when a user uses aan url that has to link mapped to it.
+app.use((request, response)=>{
+    response.status(404).sendFile('./views/404.html', {root: __dirname})
+})
+
