34abf8ab1a6efc0b3850c3eae13dfaa47a62947d
Tony Bonki <114664225+Tonjaro@users.noreply.github.com>
2023-01-26 12:33:38 +0000

    styled index page, addednav bar

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Learning-Node.js
 In this repository I will be following tutorials and learning how to use node.js
+
+Navbar template taken from https://mdbootstrap.com/docs/standard/navigation/navbar/examples-and-customization/
--- Tutorial 2/app.js
+++ Tutorial 2/app.js
@@ -16,9 +16,9 @@ app.listen(3000)
 
 app.get('/', (request, response)=>{
     const blogs = [
-        {title:'Tony becomes successful', snippet:'Lorem ipsum dolor sit amet consectetur'},
-        {title:'Jason learned that hes not human', snippet:'Lorem ipsum dolor sit amet consectetur'},
-        {title:'Micheal discovers the true meaning of life', snippet:'Lorem ipsum dolor sit amet consectetur'},
+        {title:'Sharks found in bathtub', snippet:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi repellat deleniti incidunt ex mollitia odio, at numquam omnis. Itaque nobis maiores culpa earum consequuntur eius commodi totam libero nisi veritatis?'},
+        {title:'Jason learned that hes not human', snippet:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi repellat deleniti incidunt ex mollitia odio, at numquam omnis. Itaque nobis maiores culpa earum consequuntur eius commodi totam libero nisi veritatis?'},
+        {title:'Micheal discovers the true meaning of life', snippet:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi repellat deleniti incidunt ex mollitia odio, at numquam omnis. Itaque nobis maiores culpa earum consequuntur eius commodi totam libero nisi veritatis?'},
     ]
     response.render('index', {title:'home', blogs:blogs})
 })
