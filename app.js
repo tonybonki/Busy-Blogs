de1b4f6cc8a201a6a2c206c2f0a17041d06ac4e9
Tonjaro <114664225+Tonjaro@users.noreply.github.com>
2023-02-25 23:49:40 +0000

    reset site

--- app.js
+++ app.js
@@ -1,137 +1,125 @@
-// // Using Express
-// const {
-//     request, response
-// } = require('express')
-// const express = require('express')
+// Using Express
+const {
+    request, response
+} = require('express')
+const express = require('express')
 
-// //Use path to aquire images and icons form the public folder
-// const path = require('path')
+//Use path to aquire images and icons form the public folder
+const path = require('path')
 
-// // Express app
+// Express app
 
-// const app = express()
-
-
-// // Set public static directory to allow the browser to acess the files on the server
-// // What files should be static?
-// app.use("/public", express.static(path.join(__dirname, 'public')));
+const app = express()
 
 
-// var bodyParser = require('body-parser')
-
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: false }))
-
-// // parse application/json
-// app.use(bodyParser.json())
-
-// // Acess the Blog model from blog.js
-
-// const Blog = require('./models/blog');
-
-// // Require the Mongoose Package
-
-// const mongoose = require('mongoose');
-// const { render } = require('ejs');
-// const { result } = require('lodash');
+// Set public static directory to allow the browser to acess the files on the server
+// What files should be static?
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 
-// // Ignore deprecation warnings
-// mongoose.set('strictQuery', true);
+var bodyParser = require('body-parser')
 
-// // Connect to the MongoDb
-// const dbURI = 'mongodb+srv://bonchii:wammerFishy2465@busyblogs.oaldwlg.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect(dbURI)
-//     .then((result)=>{
-//         // listen for requests 
-//         app.listen(3000)
-//     })
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 
+// Acess the Blog model from blog.js
 
+const Blog = require('./models/blog');
 
-// // register the view engine ejs automatically looks in the views folder
-// app.set('view engine', 'ejs')
+// Require the Mongoose Package
 
-
-// // Middleware that fires for ever request because it it at the top fo the file
+const mongoose = require('mongoose');
+const { render } = require('ejs');
+const { result } = require('lodash');
 
 
-// //Rounting and listening for requests
+// Ignore deprecation warnings
+mongoose.set('strictQuery', true);
 
-// app.get('/', (request, response) => {
-//     response.render('index', {})
-// })
-
-
-// app.get('/blogs', (request,response)=>{
-//     Blog.find()
-//         .then((result)=>{
-//             response.render('blogs', {blogs:result})
-//         })
-//         .catch((err)=>{
-//             console.log(err)
-//         })
-// })
-
-// app.post('/blogs', (request, response) => {
-//     const blog = new Blog(request.body)
-
-//     blog.save()
-//         .then((result) =>{
-//             response.redirect('/success')
-//         })
-// })
+// Connect to the MongoDb
+const dbURI = 'mongodb+srv://bonchii:wammerFishy2465@busyblogs.oaldwlg.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+    .then((result)=>{
+        // listen for requests 
+        app.listen(3000)
+    })
 
 
 
-// // Get a page with the object id
-// app.get('/blogs:id', (request, response) => {    
-//     const id = request.params.id;
-//     Blog.findById(id)
-//         .then(result => {
-//             response.render('blog-details', { blog: result})
-//         })
-// })
 
-// app.get('/blogs/create', (request, response) => {    
-//     response.render('create', {
-//         title: 'Create a blog'
-//     })
-// })
+// register the view engine ejs automatically looks in the views folder
+app.set('view engine', 'ejs')
 
-// app.delete('/blogs/:id', (request, response) => {
-//     const id = request.params.id
+
+// Middleware that fires for ever request because it it at the top fo the file
+
+
+//Rounting and listening for requests
+
+app.get('/', (request, response) => {
+    response.render('index', {})
+})
+
+
+app.get('/blogs', (request,response)=>{
+    Blog.find()
+        .then((result)=>{
+            response.render('blogs', {blogs:result})
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
+
+app.post('/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save()
+        .then((result) =>{
+            response.redirect('/success')
+        })
+})
+
+
+
+// Get a page with the object id
+app.get('/blogs:id', (request, response) => {    
+    const id = request.params.id;
+    Blog.findById(id)
+        .then(result => {
+            response.render('blog-details', { blog: result})
+        })
+})
+
+app.get('/blogs/create', (request, response) => {    
+    response.render('create', {
+        title: 'Create a blog'
+    })
+})
+
+app.delete('/blogs/:id', (request, response) => {
+    const id = request.params.id
     
-//     Blog.findByIdAndDelete(id)
-//         .then(result =>{
-//             response.json({ redirect: '/blogs' })
-//         })
-//         .catch(err => {console.log(err)})
-// })
+    Blog.findByIdAndDelete(id)
+        .then(result =>{
+            response.json({ redirect: '/blogs' })
+        })
+        .catch(err => {console.log(err)})
+})
 
-// // Get a sucess page when the user completes the form
-// app.get('/success', (request, response) => {    
-//     response.render('success')
-// })
+// Get a sucess page when the user completes the form
+app.get('/success', (request, response) => {    
+    response.render('success')
+})
 
 
 
-// //404 page when a user uses aan url that has to link mapped to it.
-// // This is an example of middleware
-// app.use((request, response) => {
-//     response.status(404).render('404')
-// })
+//404 page when a user uses aan url that has to link mapped to it.
+// This is an example of middleware
+app.use((request, response) => {
+    response.status(404).render('404')
+})
 
-const express = require('express');
-const app = express();
-
-app.set('port', process.env.PORT || 3000); // Set the port number to 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`Server listening on port ${app.get('port')}`);
-});
